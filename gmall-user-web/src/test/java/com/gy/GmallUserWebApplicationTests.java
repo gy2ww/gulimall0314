9a5cc01a208fb455dcc9c0b982d18464c987ea40
gaoyong <gaoyong@ithro.com.cn>
2020-03-30 10:47:49 +0800

    sku

---
+++ gmall-user-web/src/test/java/com/gy/GmallUserWebApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.gy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
